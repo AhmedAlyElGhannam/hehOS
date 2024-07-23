42f1566c295c5226f86710e847d742a747be4edd
AhmedAlyElGhannam <ahmedaly.g112@gmail.com>
2024-07-23 19:18:05 +0300

    filesystem is FINALLY alive and playing

---
+++ 2_ProtectedModeDevelopment/V010_DiskAndFileSystem/src/config.h
@@ -0,0 +1,20 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define KERNEL_CODE_SELECTOR 0x08
+#define KERNEL_DATA_SELECTOR 0x10
+
+
+#define HEHOS_TOTAL_INTERRUPTS        512
+#define HEHOS_HEAP_SIZE_BYTES           104857600 // 100MB heap size
+#define HEHOS_HEAP_BLOCK_SIZE           4096
+#define HEHOS_HEAP_ADDRESS              0x01000000 
+#define HEHOS_HEAP_TABLE_ADDRESS        0x00007E00
+
+#define HEHOS_SECTOR_SIZE               512
+
+#define HEHOS_MAX_FILESYSTEMS           12
+#define HEHOS_MAX_FILE_DESCRIPTORS      512 // max no of opened files
+
+#define HEHOS_MAX_PATH                  108
+#endif
